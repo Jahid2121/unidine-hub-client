5e722c07958a820538b68ce192432b00cebe71bd
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-25 07:41:07 +0600

    Basic Router and page setUp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-4xl font-bold text-green-700'>Got it</h2>
+    
+    </>
+  )
+}
+
+export default App
