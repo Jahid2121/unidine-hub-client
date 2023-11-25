a71362a4c91677dbcdf6ff09ceca1bec4186065e
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-25 07:57:07 +0600

    Project Initialized

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -4,7 +4,7 @@ const MainLayout = () => {
   return (
     <div>
         <Outlet />
-    </div>
+        git remote add origin https://github.com/programming-hero-web-course1/b8a12-client-side-Jahid2121.git    </div>
   );
 };
 
