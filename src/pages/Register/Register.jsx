df6f582588938e58d1e935e779dbd630225d4fac
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-26 15:00:09 +0600

    Password validation update

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -24,23 +24,45 @@ const Register = () => {
             name="email"
             {...register("email", { required: true })}
             className="input input-bordered"
-            required
           />
-          {errors.email && <span>This field is required</span>}
+          {errors.email && (
+            <span className="text-red-800">Email is required</span>
+          )}
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
+
           <input
             type="password"
-            placeholder="password"
             name="password"
-            {...register("password")}
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              maxLength: 20,
+              pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+            })}
+            placeholder="password"
             className="input input-bordered"
-            required
           />
+          {errors.password?.type === "required" && (
+            <p className="text-red-800">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-800">
+              Password must be 6 or more characters
+            </p>
+          )}
+          {errors.password?.type === "maxLength" && (
+            <p className="text-red-800">Maximum password length exceeded</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-800">
+              Password must be  include at least one uppercase letter, one lowercase letter, one special character, and one number.
 
+            </p>
+          )}
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Register</button>
