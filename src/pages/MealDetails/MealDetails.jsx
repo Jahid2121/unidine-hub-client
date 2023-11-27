36946f433ddd572a72e32fafc541b5b8d33beeda
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-27 20:00:15 +0600

    Send req meal data to the server

--- src/pages/MealDetails/MealDetails.jsx
+++ src/pages/MealDetails/MealDetails.jsx
@@ -5,66 +5,103 @@ import Btn from "../../components/Btn";
 import { AiOutlineLike } from "react-icons/ai";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
+import axios from "axios";
 
 const MealDetails = () => {
-    const meal = useLoaderData()
-    const navigate = useNavigate()
-    const location = useLocation()
-    const {user} = useAuth()
-    const {_id,title,category,image,ingredients,description,price,rating,postTime,likes,reviews,adminName,adminEmail} = meal;
+  const meal = useLoaderData();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useAuth();
+  const {
+    _id,
+    title,
+    category,
+    image,
+    ingredients,
+    description,
+    price,
+    rating,
+    postTime,
+    likes,
+    reviews,
+    adminName,
+    adminEmail,
+  } = meal;
 
-    const handleReqMeal = meal => {
-        if(user){
-            console.log(meal);
+  const handleReqMeal = (meal) => {
+    if (user) {
+      console.log(Object.keys(meal).join(","));
+      const reqMeal = {
+        title,
+        image,
+        category,
+        likes,
+        reviews,
+        adminName,
+        adminEmail,
+      };
+      axios.post('http://localhost:5000/requestedMeals', reqMeal)
+      .then(res => {
+        console.log(res.data);
+      })
+    } else {
+      Swal.fire({
+        title: "Please login for making meal request",
+        text: "",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
         }
-        else{
-            Swal.fire({
-                title: "Please login for making meal request",
-                text: "",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Login!"
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  navigate('/login', {state: {from: location}})
-                }
-              });
-        }
+      });
     }
+  };
   return (
     <>
-    <div className="flex gap-4">
-      <div className="w-1/2">
-        <img src={image} alt="" />
-        <div className="ml-8 mt-4">
+      <div className="flex gap-4">
+        <div className="w-1/2">
+          <img src={image} alt="" />
+          <div className="ml-8 mt-4">
             <hr />
-        <h3 className="text-2xl font-medium">Meal Description:</h3>
-        <p className="text-[16px]">{description}</p>
+            <h3 className="text-2xl font-medium">Meal Description:</h3>
+            <p className="text-[16px]">{description}</p>
+          </div>
         </div>
-      </div>
-      <div className="w-1/2 ml-4 mt-8">
-    <h3 className=" text-3xl font-bold">{title}</h3>
-        <Rating className="mt-5 " style={{ maxWidth: 100 }} value={rating} readOnly></Rating>
-        <p className="mt-5">Post Time: {postTime}</p>
-        <p className="mt-3 font-medium">Posted by: {adminName}</p>
-        <p className="mt-5 font-bold text-2xl">${price}</p>
-        <div className="flex items-center gap-10 ">
-        <button onClick={() =>handleReqMeal(meal)}><Btn  title="Request Meal" /></button>
-        <span className="text-2xl border  border-black p-2  rounded-full">
-        <AiOutlineLike color="green" />
-        </span>
-        </div>
+        <div className="w-1/2 ml-4 mt-8">
+          <h3 className=" text-3xl font-bold">{title}</h3>
+          <Rating
+            className="mt-5 "
+            style={{ maxWidth: 100 }}
+            value={rating}
+            readOnly
+          ></Rating>
+          <p className="mt-5">Post Time: {postTime}</p>
+          <p className="mt-3 font-medium">Posted by: {adminName}</p>
+          <p className="mt-5 font-bold text-2xl">${price}</p>
+          <div className="flex items-center gap-10 ">
+            <button onClick={() => handleReqMeal(meal)}>
+              <Btn title="Request Meal" />
+            </button>
+            <span className="text-2xl border  border-black p-2  rounded-full">
+              <AiOutlineLike color="green" />
+            </span>
+          </div>
 
-        <h3>Ingredients</h3>
-        <p>{ingredients}</p>
+          <h3>Ingredients</h3>
+          <p>{ingredients}</p>
+        </div>
       </div>
-    </div>
-    {/* reviews */}
-    <div>
-    <SectionTitle heading="reviews" subHeading="From Our member about this meal" />
-    </div>
+      {/* reviews */}
+      <div>
+        <SectionTitle
+          heading="reviews"
+          subHeading="From Our member about this meal"
+        />
+      </div>
     </>
   );
 };
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,16 +1,25 @@
 import useAuth from "../../hooks/useAuth";
 
 const Profile = () => {
-    const {user} = useAuth()
+  const { user } = useAuth();
   return (
-      <div className="text-center mt-20">
-        <img className="rounded-full w-20 mb-4 flex mx-auto" src={user?.photoURL} alt="" />
-        <p>{user?.email}</p>
-        <h2>  Welcome  <span>{user?.displayName}</span></h2>
-        <p>Your Badge </p>
-        <img className="w-11 flex mx-auto" src={"https://i.ibb.co/T2pZhJ0/medal-1.png"} alt="" />
-        
-        
+    <div className="text-center mt-20">
+      <img
+        className="rounded-full w-20 mb-4 flex mx-auto"
+        src={user?.photoURL}
+        alt=""
+      />
+      <p>{user?.email}</p>
+      <h2>
+        {" "}
+        Welcome <span>{user?.displayName}</span>
+      </h2>
+      <p>Your Badge </p>
+      <img
+        className="w-11 flex mx-auto"
+        src={"https://i.ibb.co/T2pZhJ0/medal-1.png"}
+        alt=""
+      />
     </div>
   );
 };
