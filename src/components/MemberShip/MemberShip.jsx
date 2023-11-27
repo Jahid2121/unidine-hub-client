971d81f8643b3addc8e5e07b6dbf39d3cdbedc52
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-27 09:18:38 +0600

    Package section add

--- src/components/MemberShip/MemberShip.jsx
+++ src/components/MemberShip/MemberShip.jsx
@@ -1,6 +1,7 @@
 import useMembership from "../../hooks/useMembership/useMembership";
 import Btn from "../Btn";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import { FaMedal } from "react-icons/fa";
 
 const MemberShip = () => {
   const { isPending, isError, error, memberShipData } = useMembership();
@@ -17,14 +18,18 @@ const MemberShip = () => {
       <div className="text-center">
         <SectionTitle heading="package" subHeading="Membership Packages" />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {memberShipData.map((pack) => (
-            <div key={pack.id} className="bg-white p-6 rounded-lg shadow">
-              <h3 className="text-xl font-semibold mb-4">{pack.name}</h3>
-              <p className="text-gray-700">Price: ${pack.price}</p>
-              <p className="text-gray-700">Benefits: {pack.benefits}</p>
-              <Btn title="Upgrade Now"></Btn>
-            </div>
-          ))}
+          {memberShipData.map((pack) => {
+            
+            return (
+              <div key={pack.id} className="bg-white p-6 rounded-lg shadow">
+                <img src={pack.image} className="w-44 flex mx-auto mb-3" alt={pack.name} />
+                <h3 className="text-xl font-semibold mb-4">{pack.name}</h3>
+                <p className="text-gray-700">Price: ${pack.price}</p>
+                <p className="text-gray-700">Benefits: {pack.benefits}</p>
+                <Btn title="Upgrade Now"></Btn>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
