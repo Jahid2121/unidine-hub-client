e82b4733fdb3ff0ead2fc6f0a13f7e20d5589aba
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-28 18:45:01 +0600

    Update AddMeals page

--- src/pages/AddMeals/AddMeals.jsx
+++ src/pages/AddMeals/AddMeals.jsx
@@ -1,15 +1,33 @@
 import { FaUtensilSpoon } from "react-icons/fa";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import Swal from "sweetalert2";
 import { useForm } from "react-hook-form";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const AddMeals = () => {
   const { register, handleSubmit, reset } = useForm();
   const axiosSecure = useAxiosSecure();
+  const [formattedDate, setFormattedDate] = useState(null);
+  const {user} = useAuth()
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const formattedDate = formatDate(currentDate);
+    setFormattedDate(formattedDate);
+  }, []);
+
+  function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
   const onSubmit = async (data) => {
     console.log(data);
 
-    
+   
   };
 
   return (
@@ -122,6 +140,116 @@ const AddMeals = () => {
                 className="input input-bordered w-full"
               />
             </div>
+          </div>
+
+
+          <div className="flex gap-6">
+            {/* recipe name */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">Post date</span>
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                defaultValue={formattedDate}
+                {...register("rating", { required: true })}
+                required
+                disabled
+                className="input input-bordered w-full"
+              />
+            </div>
+
+            {/* likes  */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">likes</span>
+              </label>
+              <input
+                type="number"
+                placeholder="0"
+                defaultValue={0}
+                {...register("likes", { required: true })}
+                required
+                disabled
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+          
+          <div className="flex gap-6">
+             {/* reviews  */}
+             <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">reviews</span>
+              </label>
+              <input
+                type="number"
+                placeholder="0"
+                defaultValue={0}
+                {...register("reviews", { required: true })}
+                required
+                disabled
+                className="input input-bordered w-full"
+              />
+            </div>
+
+            {/* recipe name */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">Post date</span>
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                defaultValue={formattedDate}
+                {...register("rating", { required: true })}
+                required
+                disabled
+                className="input input-bordered w-full"
+              />
+            </div>
+
+           
+          </div>
+
+
+
+
+          <div className="flex gap-6">
+             {/* name  */}
+             <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">name</span>
+              </label>
+              <input
+                type="text"
+                placeholder={`${user?.displayName}`}
+                defaultValue={`${user?.displayName}`}
+                {...register("name", { required: true })}
+                required
+                disabled
+                className="input input-bordered w-full"
+              />
+            </div>
+
+            {/*  email */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">email</span>
+              </label>
+              <input
+                type="text"
+                placeholder={`${user?.email}`}
+                defaultValue={`${user?.email}`}
+                {...register("email", { required: true })}
+                required
+                disabled
+                className="input input-bordered w-full"
+              />
+            </div>
+
+           
           </div>
 
           <button className="btn">
