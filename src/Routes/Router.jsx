1f04df0d8567bfaf2b138ecb9bf6fb27f07397b6
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-28 15:13:13 +0600

    Users route secure for admin

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -5,62 +5,69 @@ import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
 import Meals from "../pages/Meals/Meals";
 import MealDetails from "../pages/MealDetails/MealDetails";
-  import Dashboard from "../Layout/Dashboard/Dashboard";
+import Dashboard from "../Layout/Dashboard/Dashboard";
 import Profile from "../pages/Profile/Profile";
 import ReqMeals from "../pages/ReqMeals/ReqMeals";
 import MyReviews from "../pages/Myreviews/MyReviews";
 import Users from "../pages/Users/Users";
-
-
+import AdminRoute from "./AdminRoute";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout />,
-      children: [
-        {
-            path: '/',
-            element: <Home />
-        },
-        {
-          path: '/login',
-          element: <Login />
-        },
-        {
-          path: '/register',
-          element: <Register />
-        },
-        {
-          path: '/meals',
-          element: <Meals />
-        },
-        {
-          path: '/meal/:id',
-          element: <MealDetails />,
-          loader: ({params}) => fetch(`http://localhost:5000/meal/${params.id}`)
-        },
-      ]
-    },
-    {
-      path: '/dashboard',
-      element: <privateRoute><Dashboard /></privateRoute>,
-      children: [
-        {
-          path: '/dashboard',
-          element: <Profile />
-        },
-        {
-          path: 'requestedMeals',
-          element: <ReqMeals />
-        },
-        {
-          path: 'reviews',
-          element: <MyReviews />
-        },
-        {
-          path: 'users',
-          element: <Users />
-        },
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/meals",
+        element: <Meals />,
+      },
+      {
+        path: "/meal/:id",
+        element: <MealDetails />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/meal/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <AdminRoute>
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      </AdminRoute>
+    ),
+    children: [
+      {
+        path: "/dashboard",
+        element: <Profile />,
+      },
+      {
+        path: "requestedMeals",
+        element: <ReqMeals />,
+      },
+      {
+        path: "reviews",
+        element: <MyReviews />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+    ],
+  },
+]);
