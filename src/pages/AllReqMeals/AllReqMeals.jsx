cf997afad62237df3142a8a4d4a507c686074868
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-28 22:12:43 +0600

    Update AllReqMeals page

--- src/pages/AllReqMeals/AllReqMeals.jsx
+++ src/pages/AllReqMeals/AllReqMeals.jsx
@@ -5,7 +5,48 @@ const AllReqMeals = () => {
     console.log(allReqMeals);
   return (
     <div>
-      <h2>  Welcome to AllReqMeals </h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>status</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {allReqMeals.map((meal, idx) => (
+              <tr key={meal._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={meal.image} alt={meal.title} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{meal.title}</div>
+                      <div className="text-sm opacity-50">{meal.email}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {meal.name}
+                </td>
+                <td>pending</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">serve</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
