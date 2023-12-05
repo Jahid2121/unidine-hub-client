c98d24e58683063014615b499017b22c0d4acda0
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-12-05 21:23:54 +0600

    Fix but of requseted meal page data not found by changing to axiosSecure to axiosPublic

--- src/hooks/useReqMeal.jsx
+++ src/hooks/useReqMeal.jsx
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
-import useAxiosPublic from "./useAxiosPublic";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useReqMeal = () => {
-  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
   const { refetch, data: requestedMeals = [] } = useQuery({
     queryKey: ["requestedMeals", user?.email],
     queryFn: async () => {
-      const res = await axiosPublic.get(`/requestedMeals?email=${user?.email}`);
+      const res = await axiosSecure.get(`/requestedMeals?email=${user?.email}`);
       return res.data;
     },
   });
